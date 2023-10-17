7b6745bd7fe8d96fb20a7058831fa491f60f7dcf
L <david3nko_dmitriy@mail.ru>
2023-10-17 13:56:45 +0300

    added about_page

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  {path:'', component:MainPageComponent}
+  {path:'', component:MainPageComponent,},
+  {path:'about', component:AboutComponent}
 ];
 
 @NgModule({
